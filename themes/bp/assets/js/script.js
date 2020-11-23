9ed63d2836d8d56874d448529129d32c5519a5f9
Dmitriy Klimov <d.klimov@web-nanny.ru>
2020-11-23 13:11:36 +0300

    Добавлен плавный скролл к форме на главной

--- themes/bp/assets/js/script.js
+++ themes/bp/assets/js/script.js
@@ -1025,3 +1025,10 @@ $(document).on('click', '.owl-item.cloned a', function(e) {
 
     return false;
 });
+
+$("#toform").on("click", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
